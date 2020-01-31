a4cafd529e93a70b0fc69a7bb7c28c6ba4f4cd35
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2020-01-31 17:04:21 +0000

    scaled image down to make it easier to see

--- VisionDemo/Renderer.cpp
+++ VisionDemo/Renderer.cpp
@@ -11,5 +11,6 @@ void Renderer::render(std::vector<DetectedCard> cards, cv::Mat image) {
 		cv::rectangle(image, card.cardRectangle, colour);
 	}
 
+	cv::resize(image, image, cv::Size(), 0.5, 0.5);
 	cv::imshow(windowName, image);
 }
